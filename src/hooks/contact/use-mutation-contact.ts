ee1c0e8477e4a8da550505c79cef0a3cd102f54b
W Ng <wolverine25th@gmail.com>
2025-06-13 16:28:28 +0700

    feat(ui): move telegram notification to discord

--- src/hooks/contact/use-mutation-contact.ts
+++ src/hooks/contact/use-mutation-contact.ts
@@ -12,29 +12,27 @@ type TContactDTO = {
 export const useMutationContact = () => {
   const mutation = useMutation({
     mutationFn: async (dto: TContactDTO) => {
-      const botToken = "7724158646:AAEwNqexZj-dN9B4NdBBz3qa0ZRMPhHdNJI";
-      const chatId = "-4718078253"; // Replace with your chat ID if different
       const message = `
-🚀 *New Contact Form Submission* 🚀
+-------------------------------------------\n\n
+New Contact Form Submission 
 
-👤 *Name:* ${dto.name}
-📧 *Email:* ${dto.email}
-📱 *Phone:* ${dto.phone}
-💬 *Message:* 
+\n👤 Name: ${dto.name}
+\n📧 Email: ${dto.email}
+\n📱 Phone: ${dto.phone}
+\n💬 Message: 
 ${dto.message}
 
 From Recodepush Landing Page ✨
 `;
       const response = await fetch(
-        `https://api.telegram.org/bot${botToken}/sendMessage`,
+        `https://discord.com/api/webhooks/1383004995082780785/A7ms4WD8-rIQdha7_zk-o5Tm4FJfXsBe6bPlbPJdt2w6LSq2YW2H3kkhyhjzF2ROghTh`,
         {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
           },
           body: JSON.stringify({
-            chat_id: chatId,
-            text: message,
+            content: message,
           }),
         }
       );
